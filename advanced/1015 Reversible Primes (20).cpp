2e3a82e8e4797544ccbfe86f35e0299a437b5fd2
niedong <niedong0816@126.com>
2022-01-06 22:32:34 +0800

    Update advanced No.1015

--- advanced/1015 Reversible Primes (20).cpp
+++ advanced/1015 Reversible Primes (20).cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool isPrime(int n) {
     if (n <= 1) return false;
 
-    for (int i = 2; i < n; ++i) if (n % i == 0) return false;
+    for (int i = 2; i <= static_cast<int>(sqrt(n)); ++i) if (n % i == 0) return false;
     return true;
 }
 
